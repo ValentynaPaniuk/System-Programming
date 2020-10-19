d98d96a136bda4f4ffb1692541e8918fd919c27a
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2020-10-19 11:36:40 +0300

    Homework

---
+++ WorldOfTask/TankLib/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TankLib
+{
+    public class Tank
+    {
+        static Random random = new Random();
+
+        public string Name { get; set; }
+        public int Ammunition { get; set; }
+        public int Armor { get; set; }
+        public int Maneuverability { get; set; }
+
+        
+
+        public Tank(string name)
+        {
+            Name = name;
+            Ammunition = random.Next(0, 100);
+            Armor = random.Next(0, 100);
+            Maneuverability = random.Next(0, 100);
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " / Amun = " +  this.Ammunition + " / Armor = " +  this.Armor + " / Speed = " +  this.Maneuverability;
+        }
+
+        public static Tank operator ^(Tank t1, Tank t2)
+        {
+            if (t1.Ammunition > t2.Ammunition && t1.Armor > t2.Armor)
+                return t1;
+            else if (t1.Ammunition > t2.Ammunition && t1.Maneuverability > t2.Maneuverability)
+                return t1;
+            else if (t1.Armor > t2.Armor && t1.Maneuverability > t2.Maneuverability)
+                return t1;
+            else return t2;
+        }
+    }
+}
---
+++ WorldOfTask/WorldOfTask/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Documents;
+using TankLib;
+
+namespace WorldOfTask
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Tank> tanksRed;
+        ObservableCollection<Tank> tanksBlue;
+        public MainWindow()
+        { 
+          InitializeComponent();
+
+            tanksRed = new ObservableCollection<Tank>() 
+            { 
+            new Tank("T-32"),
+            new Tank("T-32"),
+            new Tank("T-32"),
+            new Tank("T-32"),
+            new Tank("T-32")
+            };
+            tanksBlue = new ObservableCollection<Tank>()
+            {
+            new Tank("Pantera"),
+            new Tank("Pantera"),
+            new Tank("Pantera"),
+            new Tank("Pantera"),
+            new Tank("Pantera")
+            };
+            lbredTanks.ItemsSource = tanksRed;
+            lbblueTanks.ItemsSource = tanksBlue;
+        }
+
+        private void btnFight_Click(object sender, RoutedEventArgs e)
+        {
+            int coutRed = 0;
+            int coutBlue = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Tank win = tanksBlue[i] ^ tanksRed[i];
+                if (win.Name == "T-32")
+                    coutRed++;
+                else
+                    coutBlue++;
+            }
+            lRed.Content = coutRed;
+            lBlue.Content = coutBlue;
+
+            if (coutRed > coutBlue)
+                MessageBox.Show("T-32 team - Win");
+            else
+                MessageBox.Show("Pantera team - Win");
+        }
+    }
+}
